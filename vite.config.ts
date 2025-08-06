87453fe5bbfdae286ed0a9da30c5269c99d1559f
bh2980 <bh2980@naver.com>
2025-08-07 08:58:20 +0900

    fix: vite path alias 수정

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+      "@/ui": path.resolve(__dirname, "./src/components/ui/index.ts"),
       "@": path.resolve(__dirname, "./src"),
     },
   },
