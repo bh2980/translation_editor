ecb48c94fdd2b40ce101cc1b1b37b5b03e2d7f76
bh2980 <bh2980@naver.com>
2025-08-07 22:52:14 +0900

    refactor: useGridStore set 함수 prev 지원

--- src/stores/grid-store.ts
+++ src/stores/grid-store.ts
@@ -4,13 +4,24 @@ import { type ColDef } from "ag-grid-community";
 interface GridStore {
   columnDefs: ColDef[];
   rowData: unknown[];
-  setColumnDefs: (columnDefs: ColDef[]) => void;
-  setRowData: (rowData: unknown[]) => void;
+  setColumnDefs: (
+    columnDefs: ColDef[] | ((prev: ColDef[]) => ColDef[])
+  ) => void;
+  setRowData: (rowData: unknown[] | ((prev: unknown[]) => unknown[])) => void;
 }
 
-export const useGridStore = create<GridStore>((set) => ({
+export const useGridStore = create<GridStore>((set, get) => ({
   columnDefs: [],
   rowData: [],
-  setColumnDefs: (columnDefs) => set({ columnDefs }),
-  setRowData: (rowData) => set({ rowData }),
+  setColumnDefs: (columnDefs) =>
+    set({
+      columnDefs:
+        typeof columnDefs === "function"
+          ? columnDefs(get().columnDefs)
+          : columnDefs,
+    }),
+  setRowData: (rowData) =>
+    set({
+      rowData: typeof rowData === "function" ? rowData(get().rowData) : rowData,
+    }),
 }));
