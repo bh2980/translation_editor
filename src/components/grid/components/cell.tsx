f340544dff9dd877e5e71182996bd34fafac7482
bh2980 <bh2980@naver.com>
2025-08-07 23:37:43 +0900

    feat: cell에 열추가 구현 및 행 추가 구현

--- src/components/grid/components/cell.tsx
+++ src/components/grid/components/cell.tsx
@@ -1,24 +1,46 @@
-import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuSeparator,
-  ContextMenuTrigger,
-} from "@/ui";
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuSeparator, ContextMenuTrigger } from "@/ui";
 import type { CustomCellRendererProps } from "ag-grid-react";
+import { useOverlay } from "@lemoncloud/react-overlay";
+import { AddColumnDialog } from "../dialogs/add-column-dialog";
+import { useGridStore } from "@/stores";
+import { insert } from "@/lib/utils";
 
 export const Cell = (props: CustomCellRendererProps) => {
+  const overlay = useOverlay();
+  const setRowData = useGridStore((state) => state.setRowData);
+
+  const addRow = (direction: "up" | "down") => {
+    const rowIndex = Number(props.node.rowIndex);
+    const targetIndex = direction === "up" ? rowIndex : rowIndex + 1;
+
+    setRowData((prev) => insert(prev, targetIndex, {}));
+  };
+
   return (
     <ContextMenu>
       <ContextMenuTrigger className="h-full w-full inline-flex px-3">
         <div>{props.value}</div>
       </ContextMenuTrigger>
       <ContextMenuContent>
-        <ContextMenuItem>왼쪽에 열 추가</ContextMenuItem>
-        <ContextMenuItem>오른쪽에 열 추가</ContextMenuItem>
+        <ContextMenuItem
+          onClick={() =>
+            overlay.open((overlayProps) => <AddColumnDialog {...overlayProps} column={props.column} direction="left" />)
+          }
+        >
+          왼쪽에 열 추가
+        </ContextMenuItem>
+        <ContextMenuItem
+          onClick={() =>
+            overlay.open((overlayProps) => (
+              <AddColumnDialog {...overlayProps} column={props.column} direction="right" />
+            ))
+          }
+        >
+          오른쪽에 열 추가
+        </ContextMenuItem>
         <ContextMenuSeparator />
-        <ContextMenuItem>위쪽에 행 추가</ContextMenuItem>
-        <ContextMenuItem>아래쪽에 행 추가</ContextMenuItem>
+        <ContextMenuItem onClick={() => addRow("up")}>위쪽에 행 추가</ContextMenuItem>
+        <ContextMenuItem onClick={() => addRow("down")}>아래쪽에 행 추가</ContextMenuItem>
       </ContextMenuContent>
     </ContextMenu>
   );
