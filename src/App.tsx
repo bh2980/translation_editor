7d2df743bfd022944e1ad7e931f4ffda65b94050
bh2980 <bh2980@naver.com>
2025-08-07 22:24:17 +0900

    refactor: useState 대신 grid store를 사용하도록 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,16 @@
-import { type ColDef } from "ag-grid-community";
 import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-  Input,
   Menubar,
   MenubarContent,
   MenubarItem,
   MenubarMenu,
   MenubarTrigger,
 } from "@/ui";
-import { useRef, useState } from "react";
-import Papa from "papaparse";
-import { downloadFile, loadFile } from "./lib/utils";
-import { Cell, Grid, HeaderCell } from "./components/grid";
+import { Grid } from "./components/grid";
+import { useGridStore } from "./stores";
+import { loadCsv, saveToCsv } from "./lib/csv";
 
 export const App = () => {
-  const [columnDefs, setColumnDefs] = useState<ColDef[]>([
-    { headerName: "", checkboxSelection: true, width: 50 },
-  ]);
-  const [rowData, setRowData] = useState<unknown[]>([]);
-
-  const columnKeyInputRef = useRef<HTMLInputElement>(null);
-  const columnNameInputRef = useRef<HTMLInputElement>(null);
-
-  const loadCsv = () =>
-    loadFile((file) => {
-      Papa.parse(file, {
-        header: true,
-        skipEmptyLines: true,
-        complete: (results) => {
-          const colDef = results.meta?.fields?.map((field) => {
-            return {
-              field: field,
-              headerName: field,
-              editable: true,
-              headerComponent: HeaderCell,
-              cellRenderer: Cell,
-            };
-          });
-
-          setColumnDefs((prev) => [...prev, ...(colDef ?? [])]);
-          setRowData(results.data);
-        },
-      });
-    });
-
-  const saveToCsv = () => {
-    const csv = Papa.unparse(rowData, {
-      columns: columnDefs.map((col) => col.field ?? ""),
-    });
-
-    const blob = new Blob([csv], { type: "text/csv" });
-
-    downloadFile(blob, "data.csv");
-  };
-
-  const addColumn = () => {
-    const key = columnKeyInputRef.current?.value;
-    const name = columnNameInputRef.current?.value;
-
-    if (!key || !name) return;
-
-    setColumnDefs((prev) => [
-      ...prev,
-      {
-        field: key,
-        headerName: name,
-        sortable: true,
-        filter: true,
-        editable: true,
-      },
-    ]);
-  };
+  const { columnDefs, rowData } = useGridStore();
 
   return (
     <div className="p-8 flex flex-col gap-4 items-baseline h-full">
@@ -90,34 +23,6 @@ export const App = () => {
           </MenubarContent>
         </MenubarMenu>
       </Menubar>
-
-      <Dialog>
-        <DialogTrigger>열 추가</DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Add Column</DialogTitle>
-          </DialogHeader>
-          <DialogDescription className="grid grid-cols-2 gap-2">
-            <label>Column Key</label>
-            <Input
-              type="text"
-              placeholder="Column Key"
-              ref={columnKeyInputRef}
-            />
-            <label>Column Name</label>
-            <Input
-              type="text"
-              placeholder="Column Name"
-              ref={columnNameInputRef}
-            />
-          </DialogDescription>
-          <DialogFooter>
-            <DialogClose>Cancel</DialogClose>
-            <DialogClose onClick={addColumn}>Add</DialogClose>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-
       <Grid
         columnDefs={columnDefs}
         rowData={rowData}
