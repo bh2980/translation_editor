2d372baafb0b9312a2379ee045f0340b664b5456
bh2980 <bh2980@naver.com>
2025-08-17 17:22:13 +0900

    feat(utils): loadFIle, downloadFile 함수 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,38 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const loadFile = (onLoad: (file: File) => void) => {
+  if (!document) {
+    return;
+  }
+
+  const fileInput = document.createElement("input");
+  fileInput.type = "file";
+  fileInput.accept = "*/*";
+  fileInput.onchange = (e: Event) => {
+    const file = (e.target as HTMLInputElement).files?.[0];
+
+    if (file) {
+      onLoad(file);
+    } else {
+      throw new Error("파일이 선택되지 않았습니다.");
+    }
+  };
+
+  fileInput.click();
+};
+
+export const downloadFile = (file: Blob | MediaSource, filename: string) => {
+  const url = URL.createObjectURL(file);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  a.click();
+  URL.revokeObjectURL(url);
+};
+
+export const isDev = () => process.env.NODE_ENV === "development" || import.meta.env.DEV;
